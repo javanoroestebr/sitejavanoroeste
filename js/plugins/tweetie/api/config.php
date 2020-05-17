081b24e4288d9f4085e180c508f493e92e4eb2fb
javanoroestebr <carlosfgo@gmail.com>
2020-05-17 14:36:51 -0300

    arquivos baixados do site

---
+++ js/plugins/tweetie/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+
+    // Consumer Key
+    define('CONSUMER_KEY', 'GUKhbR0j47ZIR2bo1LOL3CPBR');
+    define('CONSUMER_SECRET', 'R6hWL22ntw7oVdH3Byzg5tLina6LxIy95EcAJDwmkfnmhEAtAl');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '61449018-xwaRA01gwc7U1BTCQQlzkiQwE33bVtpRLquxFjIsJ');
+    define('ACCESS_SECRET', 'qdey5j2EkUmnHRTW0O9M7JElJgEa9smwySqtQHJrW3TBs');
+
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
